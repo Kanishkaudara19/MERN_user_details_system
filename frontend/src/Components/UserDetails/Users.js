175a16350368e213c589415bc07c2312520a0b40
Kanishkaudara19 <kanishkaudara2000@gmail.com>
2025-07-05 23:49:11 +0530

    User report download part done

--- frontend/src/Components/UserDetails/Users.js
+++ frontend/src/Components/UserDetails/Users.js
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Nav from "../Nav/nav";
 import axios from 'axios';
 import User from "../User/User";
+import { useReactToPrint } from 'react-to-print';
 
 const URL = "http://localhost:5000/users";
 
@@ -15,17 +16,25 @@ function Users() {
     fetchHandler().then((data) => setUsers(data.users));
   },[]);
 
+  const ComponentsRef = useRef();
+      const handlePrint = useReactToPrint({
+        contentRef: ComponentsRef,
+        documentTitle: "Users Report",
+        onAfterPrint: () => alert("User Report Successfully Downloaded !"),
+    });
+
   return (
     <div>
         <Nav/>
         <h1>User details page</h1>
-        <div>
+        <div ref={ComponentsRef}>
           { users && users.map((user, i) => (
             <div key ={i}> 
             <User user={user} />
              </div>
           )) }
         </div>
+         <button onClick={ handlePrint }>Download Report</button>
     </div>
   )
 }
